4dd7709e24fc2adf3c0d89a3fbb6910f025cea38
Tristan Cacqueray <tdecacqu@redhat.com>
2021-02-21 15:28:29 +0000

    Add initial implementation

---
+++ example/Main.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE RankNTypes #-}
+
+-- | An example usage of Pipes.PulseSimple
+module Main (main) where
+
+import Control.Monad (when)
+import Data.Binary.Get (getInt16le, isEmpty, runGet)
+import Data.Binary.Put (putInt16le, runPut)
+import qualified Data.ByteString as BS
+import Data.ByteString.Lazy (fromStrict, toStrict)
+import Data.Time.Clock (UTCTime, diffUTCTime, getCurrentTime)
+import GHC.Float (int2Float)
+import GHC.Int (Int16)
+import Pipes
+import Pipes.PulseSimple
+import Pipes.Safe
+import System.Environment (getArgs)
+
+-- | Binary decoder
+decodeSampleList :: BS.ByteString -> [Int16]
+decodeSampleList = runGet get . fromStrict
+  where
+    get = do
+      empty <- isEmpty
+      if empty
+        then return []
+        else do
+          sample <- getInt16le
+          rest <- get
+          return (sample : rest)
+
+-- | Binary encoder
+encodeSampleList :: [Int16] -> BS.ByteString
+encodeSampleList = toStrict . runPut . put
+  where
+    put [] = pure ()
+    put (x : xs) = putInt16le x >> put xs
+
+-- | Print `Noise!` when signal is detected
+consume :: MonadIO m => UTCTime -> Consumer' BS.ByteString m ()
+consume prev = do
+  now <- liftIO getCurrentTime
+  liftIO $ putStrLn $ "Last buffer received " <> show (diffUTCTime now prev) <> " second ago"
+  samples <- decodeSampleList <$> await
+  when (maximum samples > 1000) (liftIO (putStrLn "Noise!"))
+  consume now
+
+generateTone :: Int -> Int -> [Int16]
+generateTone pos size = go 0
+  where
+    note = 440
+    maxInt16 :: Int16
+    maxInt16 = maxBound
+    scale :: Float
+    scale = int2Float (fromIntegral (maxInt16 `div` 5))
+    sin16 :: Int -> Int16
+    sin16 x = round $ (* scale) $ sin $ note * 2 * pi * int2Float x / 44100
+    go :: Int -> [Int16]
+    go n
+      | n == size = []
+      | otherwise = sin16 (n + pos) : go (n + 1)
+
+-- | Create a tone
+produce :: MonadIO m => UTCTime -> Producer' BS.ByteString m ()
+produce = go 0
+  where
+    size = 1764
+    go pos prev = do
+      now <- liftIO getCurrentTime
+      liftIO $ putStrLn $ "Last buffer sent " <> show (diffUTCTime now prev) <> " second ago"
+      let samples = generateTone pos size
+      let buf = encodeSampleList samples
+      yield buf
+      go (pos + size) now
+
+main :: IO ()
+main = do
+  args <- getArgs
+  now <- getCurrentTime
+  runSafeT $
+    runEffect $ case args of
+      ["record"] -> readPulse "test" Nothing 25 >-> consume now
+      ["play"] -> produce now >-> writePulse "test" Nothing 25
+      _ -> error "usage: record|play"
--- src/Pipes/PulseSimple.hs
+++ src/Pipes/PulseSimple.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
+
 -- |
 -- Copyright: (c) 2021 Tristan de Cacqueray
 -- SPDX-License-Identifier: Apache-2.0
@@ -5,9 +8,97 @@
 --
 -- Pipes for simple-pulse audio
 module Pipes.PulseSimple
-  ( someFunc,
+  ( readPulse,
+    writePulse,
   )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+import Data.ByteString (ByteString)
+import Data.Maybe (fromMaybe)
+import Pipes (Consumer', MonadIO, Producer', await, liftIO, yield)
+import Pipes.Safe (MonadSafe, bracket)
+import Sound.Pulse.Simple
+  ( BufferAttr (..),
+    Direction (..),
+    Endian (..),
+    SampleFormat (..),
+    SampleSpec (..),
+    Simple,
+    simpleFree,
+    simpleNew,
+    simpleReadRaw,
+    simpleWriteRaw,
+  )
+
+defaultSampleSpec :: SampleSpec
+defaultSampleSpec = SampleSpec (S16 LittleEndian) 44100 1
+
+-- | Return the bits count of a single sample
+sampleSize :: SampleFormat -> Int
+sampleSize = \case
+  U8 _ -> 1
+  S16 _ -> 2
+  S24 _ -> 3
+  S2432 _ -> 4
+  S32 _ -> 4
+  F32 _ -> 4
+
+-- | Return the buts count of an one second buffer
+bufferSize :: SampleSpec -> Int
+bufferSize (SampleSpec fmt sampling chan) = sampleSize fmt * sampling * chan
+
+-- | Compute the sample spec and buffer size for a given fps
+specSize :: Maybe SampleSpec -> Int -> (SampleSpec, Int)
+specSize specm fps = (spec, size)
+  where
+    spec = fromMaybe defaultSampleSpec specm
+    size = bufferSize spec `div` fps
+
+newClient :: MonadIO m => String -> Direction -> SampleSpec -> Int -> m Simple
+newClient name dir spec size = liftIO create
+  where
+    create = simpleNew Nothing name dir Nothing "pulse-pipe" spec Nothing bufAttr
+    bufAttr = Just (BufferAttr (Just size) (Just size) (Just size) (Just size) (Just size))
+
+freeClient :: MonadIO m => Simple -> m ()
+freeClient = liftIO . simpleFree
+
+-- | Create an audio pipe producer that yields `fps` bytestring buffer per second
+readPulse ::
+  MonadSafe m =>
+  -- | The client name
+  String ->
+  -- | The sample spec, default to mono 44100 Hz 16 bit signed integer
+  Maybe SampleSpec ->
+  -- | Frames per second
+  Int ->
+  -- | The pipe producing bytestring
+  Producer' ByteString m ()
+readPulse name specm fps = bracket (newClient name Record spec size) freeClient producePulse
+  where
+    (spec, size) = specSize specm fps
+    producePulse s = do
+      buf <- liftIO (simpleReadRaw s size)
+      yield buf
+      producePulse s
+{-# INLINEABLE readPulse #-}
+
+-- | Create an audio pipe consumer that awaits `fps` bytestring buffer per second
+writePulse ::
+  MonadSafe m =>
+  -- | The client name
+  String ->
+  -- | The sample spec, default to mono 44100 Hz 16 bit signed integer
+  Maybe SampleSpec ->
+  -- | Frames per second
+  Int ->
+  -- | The pipe consuming bytestring
+  Consumer' ByteString m ()
+writePulse name specm fps = bracket (newClient name Play spec size) freeClient consumePulse
+  where
+    (spec, size) = specSize specm fps
+    consumePulse s = do
+      buf <- await
+      liftIO (simpleWriteRaw s buf)
+      consumePulse s
+{-# INLINEABLE writePulse #-}
